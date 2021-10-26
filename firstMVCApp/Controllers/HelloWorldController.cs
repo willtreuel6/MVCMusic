daa006c2234da19e4040630f339a93227bb7b595
student <student@830-5CG930BB1R>
2021-10-26 10:38:14 -0400

    Add project files.

---
+++ firstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace firstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index() //Get  -- /HelloWorld/
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1) //Get -- /HelloWorld/Welcome/
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ firstMVCApp/Data/firstMVCAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using firstMVCApp.Models;
+
+namespace firstMVCApp.Data
+{
+    public class firstMVCAppContext : DbContext
+    {
+        public firstMVCAppContext (DbContextOptions<firstMVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
